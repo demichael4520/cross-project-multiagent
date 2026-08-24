25afc0d5540de0e834a1efeb58d889fda12f6b5d
demichael4520 <demichael4520@gmail.com>
2026-08-24 14:17:36 +0000

    Suppress vertexai.Client deprecation warning and protobuf version warnings for clean execution

--- deploy_concierge_adk.py
+++ deploy_concierge_adk.py
@@ -16,6 +16,9 @@ import argparse
 import os
 os.environ["GOOGLE_API_USE_CLIENT_CERTIFICATE"] = "false"
 os.environ["GOOGLE_API_USE_MTLS_ENDPOINT"] = "never"
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning, message=".*vertexai.Client class is deprecated.*")
+warnings.filterwarnings("ignore", category=UserWarning, message=".*Protobuf gencode version.*")
 import vertexai
 from dotenv import load_dotenv
 
